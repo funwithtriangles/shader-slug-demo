e40ce412bfafca62322787e2446a120881cae12f
Alex Kempton <alex@funwithtriangles.net>
2025-09-07 10:10:22 +0200

    theatre.js basic setup

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,21 @@
-import { engineStore } from './engine'
+import { engineStore } from "./engine";
+import "@theatre/core";
+import { getProject, types } from "@theatre/core";
+import studio from "@theatre/studio";
 
-const lfoInputEnabledNodeId = '40fed25628e9750c'
-const saturationNodeId = 'bff7540d69fbd802'
-const sphereScaleNodeId = '0434cc9f5cc59b70'
+const { updateNodeValue } = engineStore.getState();
 
-const satMinNodeId = `${saturationNodeId}-sliderMin`
-const satMaxNodeId = `${saturationNodeId}-sliderMax`
-const sphereScaleMinNodeId = `${sphereScaleNodeId}-sliderMin`
-const sphereScaleMaxNodeId = `${sphereScaleNodeId}-sliderMax`
+studio.initialize();
 
-document.addEventListener('click', () => {
-  const state = engineStore.getState()
-  const nodeVal = state.nodeValues[lfoInputEnabledNodeId] as boolean
+const CAM_Y = "ff6d8b05f5f49617";
 
-  state.updateNodeValue(lfoInputEnabledNodeId, !nodeVal)
-})
+const project = getProject("Shader Slug");
+const sheet = project.sheet("Scene");
 
-document.addEventListener('mousemove', (e) => {
-  const state = engineStore.getState()
-  const satMin = state.nodeValues[satMinNodeId] as number
-  const satMax = state.nodeValues[satMaxNodeId] as number
-  const saturation = (e.clientX / window.innerWidth) * (satMax - satMin) + satMin
+const cameraObj = sheet.object("Camera", {
+  camY: types.number(0, { range: [-5, 5] }),
+});
 
-  const sphereScaleMin = state.nodeValues[sphereScaleMinNodeId] as number
-  const sphereScaleMax = state.nodeValues[sphereScaleMaxNodeId] as number
-  const sphereScale =
-    (e.clientY / window.innerHeight) * (sphereScaleMax - sphereScaleMin) + sphereScaleMin
-
-  state.updateNodeValue(sphereScaleNodeId, sphereScale)
-  state.updateNodeValue(saturationNodeId, saturation)
-})
+cameraObj.onValuesChange((v) => {
+  updateNodeValue(CAM_Y, v.camY);
+});
