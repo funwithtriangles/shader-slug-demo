b209836860d7bf5be98e6c380cee1cf37e8f9bd2
Alex Kempton <alex@funwithtriangles.net>
2025-10-03 00:56:00 +0200

    vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,25 @@
 import { defineConfig } from "vite";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+// __dirname shim for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      // remap to new threejs version
+      "three/addons": "three/examples/jsm",
+      "three/webgpu": path.resolve(
+        __dirname,
+        "node_modules/three/build/three.webgpu.js"
+      ),
+      "three/tsl": path.resolve(
+        __dirname,
+        "node_modules/three/build/three.tsl.js"
+      ),
+    },
+  },
   assetsInclude: ["**/*.glb", "**/*.gltf", "**/*.hdr", "**/*.mp3"],
 });
