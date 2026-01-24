1b2cf4c12dc1f9593718d6a4b11c995dbfd4d51a
Alex Kempton <alex@funwithtriangles.net>
2026-01-24 14:58:02 +0100

    fun node controls

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -4,6 +4,20 @@ import { PiFaders } from "react-icons/pi";
 import { ControlGrid, NodeContainer } from "@hedron-gl/ui-core";
 
 const CRUSH_ID = "12617f97ba0acdde";
+const COS_ID = "4b4563d5bb481028";
+const SIN_ID = "94b4563d5bb48102";
+const WAVE_LENGTH_ID = "d0694b4563d5bb48";
+const WAVE_AMP_ID = "5d0694b4563d5bb4";
+const OPACITY_ID = "27e122b2c069676e";
+
+const nodeIds = [
+  CRUSH_ID,
+  COS_ID,
+  SIN_ID,
+  WAVE_LENGTH_ID,
+  WAVE_AMP_ID,
+  OPACITY_ID,
+];
 
 interface ControlsProps {
   isOpen: boolean;
@@ -24,7 +38,9 @@ function Controls({ isOpen, onToggle }: ControlsProps) {
       {isOpen && (
         <div className={c.panel} onClick={(e) => e.stopPropagation()}>
           <ControlGrid>
-            <NodeContainer nodeId={CRUSH_ID} layout="vertical" />
+            {nodeIds.map((id) => (
+              <NodeContainer key={id} nodeId={id} layout="vertical" />
+            ))}
           </ControlGrid>
         </div>
       )}
